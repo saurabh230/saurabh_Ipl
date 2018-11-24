15deb7bdbf6fbb28c4de7d4e9506a2a4f024ee54
saurabh <rkumarsinghsaurabh@gmail.com>
2018-11-24 11:47:56 +0530

    final soln of ipl

--- problem4.js
+++ problem4.js
@@ -1,46 +1,78 @@
 var fs = require('fs')
-var csv = require('fast-csv')
-require('./IdOfAnySeason.js')();
-var deliveriesStream = fs.createReadStream('deliveries.csv');
-var economicalBwrsPerSeason = {};
-var blrAllRuns={};
-var blrAllBolls={};
-var season = '2015'
-var matchesIn2015 = getIdOfAnySeason(season);
-csv
-.fromStream(deliveriesStream, {headers : true})
-.on("data", function(delivery){
-   if (existInArray(delivery['match_id'],matchesIn2015)){
-    let runPerBall=parseInt(delivery['total_runs'])-parseInt(delivery['bye_runs'])-parseInt(delivery['legbye_runs']);
-    if (blrAllRuns[delivery['bowler']] !== undefined){
-        blrAllRuns[delivery['bowler']] +=runPerBall;
-     } else {
-        blrAllRuns[delivery['bowler']] =runPerBall;
-     }
-     if (blrAllBolls[delivery['bowler']] !== undefined){
-        blrAllBolls[delivery['bowler']] +=1;
-     } else {
-        blrAllBolls[delivery['bowler']] =1;
-     }
-
-     
-
-
-
-   }
-
-   for(var x in blrAllBolls){
-       blrAllOvers[x]=blrAllBolls[x]/6;
-   }
-})
-.on("end", function(){
-    console.log(blrAllOvers);
-});
-function existInArray(value,array){
-    var count=array.length;
-    for(var i=0;i<count;i++){
-        if(array[i] === value)
-            return true;
-    }
-    return false;
+var match = fs.readFileSync('matches.csv', 'utf8');
+var lineArrOfMatches = match.split('\n');
+var delivery = fs.readFileSync('deliveries.csv', 'utf8');
+var lineArrOfDelivery = delivery.split('\n');
+var matches = [];
+for(var rowsInMatch = 1; rowsInMatch < lineArrOfMatches.length; rowsInMatch++){
+   matches[rowsInMatch] = lineArrOfMatches[rowsInMatch].split(',');
 }
+var deliveries = [];
+for(var rowsIndelivery = 1; rowsIndelivery < lineArrOfDelivery.length; rowsIndelivery++){
+   deliveries[rowsIndelivery] = lineArrOfDelivery[rowsIndelivery].split(',');
+}
+var year='2015';
+var economicalTopBowler = {};
+var objOfMatchIdSeason = getKeyValueFromMatch();
+function getKeyValueFromMatch(){
+    var tempObj = {};
+    for(var i = 1; i < matches.length; i++){
+        tempObj[matches[i][0]] = matches[i][1];
+    }
+    return tempObj;
+}
+var bowlers = getBowlersOfAnySeason();
+var bowlerObj = {};
+for(var i = 0; i < bowlers.length; i++){
+   var runs = 0;
+   var del = 0;
+   for(var j = 1; j < deliveries.length; j++){
+       if(bowlers[i] === deliveries[j][8] && objOfMatchIdSeason[deliveries[j][0]] === year){
+           del++;
+           runs = runs+parseInt(deliveries[j][9])+parseInt(deliveries[j][10])+parseInt(deliveries[j][13])+parseInt(deliveries[j][15]);
+       }
+   }
+   var overs = del/6;   
+    bowlerObj[bowlers[i]] = (runs/overs);
+}
+function sortValues(){
+    var sortBwlr = [];
+for (var bwlr in bowlerObj) {
+    sortBwlr.push([bwlr, bowlerObj[bwlr]]);
+}
+sortBwlr.sort(function(a, b) {
+    return a[1] - b[1];
+});
+    return sortBwlr;
+}
+
+function getBowlersOfAnySeason(){
+    var tempArr = [];
+    for(var i = 1; i < deliveries.length; i++){
+        if(objOfMatchIdSeason[deliveries[i][0]] === year){
+            tempArr.push(deliveries[i][8]);
+        }
+    }
+    var bowlerArr = [];
+    for(var i = 0; i < tempArr.length; i++){
+        var count = 0;
+        for(var j = i+1; j < tempArr.length; j++){
+            if(tempArr[i] === tempArr[j]){
+                count++;
+                break;
+            }
+        }
+        if(count === 0)
+            bowlerArr.push(tempArr[i]);
+    }
+    return bowlerArr;
+ }
+ 
+var sortBwlr = sortValues();
+for(var i = 0; i < 10; i++){    
+    var str = sortBwlr[i].toString();
+    var temp = str.split(',');
+    economicalTopBowler[temp[0]] = temp[1];
+}
+console.log('====Economical Top Bowler Of Season 2015====')
+console.log(economicalTopBowler);
